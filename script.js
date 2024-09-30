2c0dfb3f917e44f10a78e58d3ce424bfcde5b5fe
math3wsl3vi <levimathews9841@gmail.com>
2024-09-30 17:02:13 +0300

    hehe

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ function closeMenu(){
     sideMenu.style.transform = 'translateX(16rem)';
 }
 
-window.addEventListener('scroll', ()=>{
-    if(scrollY > 50){
-        navBar.classList.add('bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
-        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent', 'text-white')
-    }
-    else{
-        navBar.classList.remove('bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
-        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent', 'text-white')
+// window.addEventListener('scroll', ()=>{
+//     if(scrollY > 50){
+//         navBar.classList.add('bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
+//         navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent', 'text-white')
+//     }
+//     else{
+//         navBar.classList.remove('bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
+//         navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent', 'text-white')
 
-    }
-})
+//     }
+// })
 
 //light mode and dark mode
 
